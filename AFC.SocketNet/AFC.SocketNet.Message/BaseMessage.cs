fc13f8d78d517bfe84f3ea73bd9f4f5d0a41a893
wikey <517537691@qq.com>
2018-06-14 17:50:16 +0800

    first commit:add the project
    add first project source

---
+++ AFC.SocketNet/AFC.SocketNet.Message/BaseMessage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFC.SocketNet.Message
+{
+    public class BaseMessage
+    {
+        #region  字段/属性
+        private byte[] data;
+
+
+        #endregion
+
+
+        public BaseMessage()
+        {
+
+        }
+
+        public BaseMessage(byte[] buf,int index=0)
+        {
+
+        }
+
+        /// <summary>
+        /// 解包
+        /// </summary>
+        public virtual void Decode()
+        {
+
+        }
+
+        /// <summary>
+        /// 生成数据包
+        /// </summary>
+        public virtual void Encode()
+        {
+
+        }
+    }
+}
