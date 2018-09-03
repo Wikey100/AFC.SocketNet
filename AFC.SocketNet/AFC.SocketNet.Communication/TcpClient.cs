1691759bfda04907214c4c69de75ebbf6e3c610c
wikey <517537691@qq.com>
2018-09-03 23:56:36 +0800

    类库更新
    1、删除客户端类库
    2、增加处理主类
    3、增加TCP服务类与客户端处理类

---
+++ AFC.SocketNet/AFC.SocketNet.Communication/TcpClient.cs
@@ -0,0 +1,21 @@
+﻿/*******************************************************************
+ * * 文件名： TcpClient.cs
+ * * 文件作用： TCP客户端类
+ * *
+ * *-------------------------------------------------------------------
+ * *修改历史记录：
+ * *修改时间      修改人    修改内容概要
+ * *2017-04-22    xwj       新增
+ * *******************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFC.SocketNet.Communication
+{
+    public class TcpClient
+    {
+    }
+}
---
+++ AFC.SocketNet/AFC.SocketNet.Communication/TcpServer.cs
@@ -0,0 +1,22 @@
+﻿/*******************************************************************
+ * * 文件名： TcpServer.cs
+ * * 文件作用： TCP服务类
+ * *
+ * *-------------------------------------------------------------------
+ * *修改历史记录：
+ * *修改时间      修改人    修改内容概要
+ * *2017-04-22    xwj       新增
+ * *******************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFC.SocketNet.Communication
+{
+    public class TcpServer
+    {
+
+    }
+}
